f7055a7a8c996275fef28a5fda2df7360210c6f6
Lewis-Tsai <A38050787@gmail.com>
2022-05-29 17:02:22 +0800

    login scene, loading and stage 1
    1. add several assets(fonts, UI pictures, audios)
    2. implement login scene, loading scene
    3. change firebase database rule
    4. create required nodes for stage 1

---
+++ assets/Script/loading_controller.js
@@ -0,0 +1,48 @@
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                this.schedule(function() {  
+                    cc.director.loadScene("Menu");
+                }, 3, 0);
+            } else {
+                this.schedule(function() {  
+                    cc.director.loadScene("Login");
+                }, 3, 0);
+            }
+        });
+    },
+
+    // update (dt) {},
+});
---
+++ assets/Script/login_controller.js
@@ -0,0 +1,107 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        Email_Edit_Box: cc.EditBox,
+        Password_Edit_Box: cc.EditBox,
+        UserName_Edit_Box: cc.EditBox,
+        Login_Button: cc.Button,
+        Signup_Button: cc.Button,
+        curr_email: '',
+        curr_password: '',
+        curr_user_name: '',
+    },
+
+    onLoad () {
+        this.Login_Button.node.on('click', this.login_handler, this);
+        this.Signup_Button.node.on('click', this.signup_handler, this);
+    },
+
+    start () {
+    },
+
+    email_type_finish(){
+        if(this.Email_Edit_Box.string == "")
+            alert("email can't be blank");
+        else
+            curr_email = this.Email_Edit_Box.string;
+    },
+
+    password_type_finish(){
+        if(this.Password_Edit_Box.string == "")
+            alert("password can't be blank");
+        else
+            curr_password = this.Password_Edit_Box.string;
+    },
+    
+    username_type_finish(){
+        if(this.UserName_Edit_Box.string == "")
+            alert("user name can't be blank");
+        else{
+            curr_user_name = this.UserName_Edit_Box.string;
+            //console.log(curr_user_name);
+        }  
+    },
+
+    login_handler: function (event) {
+        //这里的 event 是一个 EventCustom 对象，你可以通过 event.detail 获取 Button 组件
+        var button = event.detail;
+        //do whatever you want with button
+        //另外，注意这种方式注册的事件，也无法传递 customEventData
+        //alert(button)
+        firebase.auth().signInWithEmailAndPassword(curr_email, curr_password)
+        .then(function(result) {
+            //cc.director.loadScene("Menu");
+            profile_path = curr_email.replace('.', '_');
+            var ProfileRef = firebase.database().ref('profile/' + profile_path);
+            ProfileRef.update({
+                email: curr_email,
+                user_name: curr_user_name,
+            })
+            .then(function () {
+                console.log("profile data upload success");
+                cc.director.loadScene("Menu");
+            }).catch(function (error) {
+                alert(error);
+            });
+        }).catch(function(error) {
+            alert(error);
+        });
+    },
+
+    signup_handler: function (event) {
+        //这里的 event 是一个 EventCustom 对象，你可以通过 event.detail 获取 Button 组件
+        var button = event.detail;
+        //do whatever you want with button
+        //另外，注意这种方式注册的事件，也无法传递 customEventData
+        //alert('here')
+        firebase.auth().createUserWithEmailAndPassword(curr_email, curr_password)
+        .then(function(result) {
+            //cc.director.loadScene("Menu");
+            profile_path = curr_email.replace('.', '_');
+            var ProfileRef = firebase.database().ref('profile/' + profile_path);
+            ProfileRef.once('value', function(snapshot) {
+                if(snapshot.val() == null){
+                    console.log('no data');
+                    ProfileRef.update({
+                        email: curr_email,
+                        user_name: curr_user_name,
+                        score: 0,
+                    })
+                    .then(function () {
+                        console.log("profile data upload success");
+                        cc.director.loadScene("Menu");
+                    }).catch(function (error) {
+                        alert(error);
+                    });
+                }  
+                else{
+                    console.log('data exist');
+                    cc.director.loadScene("Menu");
+                }
+            });
+        }).catch(function(error) {
+            alert(error);
+        });
+    }
+});
