06c33f922e57f3a4dc16fca9fdc40400ad56e88b
s109000114 <successken2001@gmail.com>
2022-06-07 21:49:32 +0800

    tank_enemies shoot
    tank_enemy can shoot the player in the correct vector

--- assets/Script/tank_enemies_bullet.ts
+++ assets/Script/tank_enemies_bullet.ts
@@ -3,23 +3,37 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class tank_enemies_bullet extends cc.Component {
 
+    @property(cc.Node)
+    player:cc.Node = null; // can't use in a prefab?
+
     private anim = null;
 
-    public init(node: cc.Node) 
+    public init(node: cc.Node, x: number, y: number, angle_x: number, angle_y: number) 
     {
+        console.log('x = ',x);
         this.anim = this.getComponent(cc.Animation);
-        this.setInitPos(node);
+        //this.setInitPos(node);
+        this.node.x = x;
+        this.node.y = y;
         this.anim.play('tank_enemy_bullet');
+        let action = cc.moveBy(1, angle_x, angle_y);
+        this.node.runAction(action);
+        //this.fire_bullet(x, y);
         //let action = cc.moveBy(1,-500,0);
         //this.node.runAction(action);
     }
 
-    private fire_bullet(){
-        //let action = cc.moveBy(1,-500,0);
+    private fire_bullet(x: number, y:number){
+        var dx = this.player.x - x;
+        var dy = this.player.y - y;
+        let action = cc.moveBy(1,dx,dy);
+        this.node.runAction(action);
+        console.log('gigigigiigigigigig');
     }
 
     setInitPos(node: cc.Node){
-        this.node.x = node.x - 50;
+        //this.node.x = node.x - 50;
+        //this.node.x = this.gun.bullet_x;
         this.node.y = node.y;
     }
 
--- assets/Script/tank_enemies_gun.ts
+++ assets/Script/tank_enemies_gun.ts
@@ -1,7 +1,7 @@
 const {ccclass, property} = cc._decorator;
 
 @ccclass
-export default class tank_enemies_gun extends cc.Component {
+export class tank_enemies_gun extends cc.Component {
     private anim = null; //this will use to get animation component
     private animateState = null; //this will use to record animationState
     @property(cc.Prefab)
@@ -9,10 +9,13 @@ export default class tank_enemies_gun extends cc.Component {
     @property(cc.Node)
     player:cc.Node = null;
 
+    public bullet_x: number = 30;
+    public bullet_y: number = 30;
+
     update(dt)
     {
         this.playerMovement(dt);
-        this.playerAnimation();
+        //this.playerAnimation();
     }
     start() 
     {
@@ -54,21 +57,24 @@ export default class tank_enemies_gun extends cc.Component {
         var dx = this.player.x - this.node.x;
         var dy = this.player.y - this.node.y;
         var dir = cc.v2(dx,dy);
-        var angle = dir.signAngle(cc.v2(1,0));
+        var angle = dir.signAngle(cc.v2(1,0)); //in radiant
         var degree = angle / Math.PI * 180;
         this.node.rotation = degree + 160;
-    }
-    private playerAnimation(){
+        //this.bullet_x = this.node.x + 67*Math.cos(angle);
         if(this.kDown){
-            //virsion 1
-            /*var bullet = cc.instantiate(this.tank_enemy_bullet_Prefab);
-            bullet.setPosition(this.node.x,this.node.y,0);
-            let action = cc.moveBy(1,-200,0);
-            bullet.runAction(action);
-            cc.find("Canvas").addChild(bullet);*/
             var bullet = cc.instantiate(this.tank_enemy_bullet_Prefab);
-            bullet.getComponent('tank_enemies_bullet').init(this.node);
+            bullet.getComponent('tank_enemies_bullet').init(
+                this.node, this.node.x+67*Math.cos(angle), this.node.y-67*Math.sin(angle)+10, 
+                this.player.x - (this.node.x+67*Math.cos(angle)), this.player.y-(this.node.y-67*Math.sin(angle)+10));
             cc.find("Canvas").addChild(bullet);
         }
+        //console.log(this.bullet_x);
     }
+    /*private playerAnimation(){
+        if(this.kDown){
+            var bullet = cc.instantiate(this.tank_enemy_bullet_Prefab);
+            bullet.getComponent('tank_enemies_bullet').init(this.node,this.bullet_x);
+            cc.find("Canvas").addChild(bullet);
+        }
+    }*/
 }
