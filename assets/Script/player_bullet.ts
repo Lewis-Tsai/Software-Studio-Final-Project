27eb2e5830ed2771614d2b57491f7bb85bd0873d
PCK64 <106243370+PCK64@users.noreply.github.com>
2022-06-19 19:51:38 +0800

    add explosion effect to player bullets

---
+++ assets/Script/player_bullet.ts
@@ -0,0 +1,39 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class player_bullet extends cc.Component {
+
+    @property(cc.Prefab)
+    explode_prefab: cc.Node = null;
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+
+    }
+
+    update (dt) {
+
+    }
+
+    onBeginContact(contact, self, other) {
+        if(other.node.name == "background_size") {
+            var explode = cc.instantiate(this.explode_prefab);
+            explode.setPosition(this.node.position.x + this.node.parent.position.x, -200);
+            cc.find("Canvas").addChild(explode);
+            self.node.destroy();
+            /*this.scheduleOnce( function() {
+                explode.destroy();
+            }, 0.15);*/
+        }
+    }
+}
